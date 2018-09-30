5131b70146389c2c64bf7396c14c1f9f54e75a80
GiaPrograms <legianguyen1999@gmail.com>
2018-09-30 18:51:40 -0400

    One file to test part 2

--- CollectionTesting.java
+++ CollectionTesting.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList; 
 import java.util.Random;
+import java.util.*;
 
 public class CollectionTesting {
     
@@ -15,8 +17,36 @@ public class CollectionTesting {
         }
 
     }
+
+    public static void genList(int size){
+       
+        ArrayList<Integer> alist= new ArrayList<Integer>();
+        int random;
+ 
+         for(int i = 0; i < size; i++){
+             
+            random = new Random().nextInt(10);
+             
+             alist.add(random);    
+         }   
+    }
+
+    public static void genVector(int size){
+       
+        Vector<Integer> v = new Vector<Integer>();
+ 
+         for(int i = 0; i < size; i++){
+             
+            int random = new Random().nextInt(10);
+             
+            v.add(random); 
+         }   
+    }
+
+
     public static void main(String[] args){
 
+        /*
         double start = System.nanoTime();
         genArray(100000000);
         genArray(100000000);
@@ -24,6 +54,24 @@ public class CollectionTesting {
         Double total = end - start;
         total= total/1000000000.0;
         System.out.println("The total time for array is " + total);
+        System.out.println("============================================================================");
+        */
 
+        double start2 = System.nanoTime();
+        genList(100000000);
+        double end2 = System.nanoTime();
+        Double total2 = end2 - start2;
+        total2= total2/1000000000.0;
+        System.out.println("The total time for arrayList is " + total2);
+        System.out.println("============================================================================");
+
+        /*
+        double start3 = System.nanoTime();
+        genVector(100000000);
+        double end3 = System.nanoTime();
+        Double total3 = end3 - start3;
+        total3= total3/1000000000.0;
+        System.out.println("The total time for vector is " + total3);
+        */
     }
 }
